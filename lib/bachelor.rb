931b7e5ea727244bdfbf93282ad84966029d8e0e
Adam Gordon <adamjgordon24@gmail.com>
2019-02-08 07:22:46 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -58,10 +58,9 @@ def get_average_age_for_season(data, season)
         info.each do |aspect, reading|
           age_pool = 0 
           age_pool += info["age"].to_i
-          average_age = (age_pool/(info.length))
+          return age_pool/(info.length)
         end
       end
     end
   end
-  average_age
 end
