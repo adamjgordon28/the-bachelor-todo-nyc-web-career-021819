4736fd23692a699fb6df2e9a41a811e649d692b6
Adam Gordon <adamjgordon24@gmail.com>
2019-02-08 07:16:58 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -54,7 +54,8 @@ end
 def get_average_age_for_season(data, season)
   data.each do |show_season,contestant|
     contestant.each do |info|
-      if info[""]
+      if show_season == season 
+      end
     end
   end
 end
