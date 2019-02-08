9dfe69c4c74c3fbc5059ee4c75036595be202493
Adam Gordon <adamjgordon24@gmail.com>
2019-02-08 07:08:18 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -29,10 +29,8 @@ def count_contestants_by_hometown(data, hometown)
   counter = 0  
   data.each do |show_season, contestant|
     contestant.each do |info|
-      info.each do |aspect, reading|
         if info["hometown"] == hometown
           counter += 1 
-        end
       end
     end
   end
