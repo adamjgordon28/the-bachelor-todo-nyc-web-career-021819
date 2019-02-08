9394056c1413b91a5d18f8e679f156ecc3b783c1
Adam Gordon <adamjgordon24@gmail.com>
2019-02-08 06:57:49 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -2,7 +2,8 @@ def get_first_name_of_season_winner(data, season)
   data.each do |show_season, contestant|
    if show_season == season 
      contestant.each do |info|
-       
+       info.each do |aspect, reading|
+       end
      end
    end
   end
